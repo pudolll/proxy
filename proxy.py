9a152c9473c79cc0c9f2a75e49a31fc794d7dd68
pudol <94850075+pudolll@users.noreply.github.com>
2025-06-08 12:20:35 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -2,12 +2,14 @@ from flask import Flask, request, redirect
 import requests
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'localhost:5000'
 
 @app.route("/<type>/<site>/<path:subpath>")
 def test(site, type, subpath):
-    url = request.url.replace(f"{site}/{type}/", "").replace(app.config['SERVER_NAME'], site)
+    url = request.url.replace(f"{type}/{site}", "")
+    url = url.replace(request.url_root, site)
+    if not url.startswith("https://"): url = "https://" + url
     match type:
+        case "url": return url
         case "redirect": return redirect(url)
         case "json": return requests.get(url).json()
 
