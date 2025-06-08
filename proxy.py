b00dda704ca852c87f8731e27a4edbe44e8b44bb
pudol <94850075+pudolll@users.noreply.github.com>
2025-06-08 11:55:51 +0200

    added proxy.py

---
+++ proxy.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect
+import requests
+
+app = Flask(__name__)
+app.config['SERVER_NAME'] = 'localhost:5000'
+
+@app.route("/<type>/<site>/<path:subpath>")
+def test(site, type, subpath):
+    url = request.url.replace(f"{site}/{type}/", "").replace(app.config['SERVER_NAME'], site)
+    match type:
+        case "redirect": return redirect(url)
+        case "json": return requests.get(url).json()
+
+app.run()
